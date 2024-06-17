89b4d6cd56a0244d8ac589dbd6aae809b38eaeca
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-06-17 21:22:52 +0100

    Add JSON support
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- mfh/__main__.py
+++ mfh/__main__.py
@@ -10,6 +10,7 @@ import sys
 import os
 import argparse
 import colorama
+import json
 
 def print_error(error):
     print(f"[ {colorama.Fore.RED}X{colorama.Style.RESET_ALL} ] {error}")
@@ -17,7 +18,7 @@ def print_error(error):
 def print_success(success):
     print(f"[ {colorama.Fore.GREEN}✓{colorama.Style.RESET_ALL} ] {success}")
 
-def generate_hash_file(file_path, head_lines, tail_lines, output, return_output=False):
+def generate_hash_file(file_path, head_lines, tail_lines, output, return_output=False, note=None, json=False):
     file_name = os.path.basename(file_path)
     file_size = os.path.getsize(file_path)
     with open(file_path, 'rb') as file:
@@ -26,92 +27,114 @@ def generate_hash_file(file_path, head_lines, tail_lines, output, return_output=
         file_lines = file_data.decode('utf-8', errors='ignore').split('\n')
         head_lines_data = file_lines[:head_lines]
         tail_lines_data = file_lines[-tail_lines:]
+        if json == True:
+            hash_file = "MFH v1\n"
+            hash_file += f"fn:{file_name}\n"
+            hash_file += f"ln:{len(file_lines)}\n"
+            hash_file += f"fs:{file_size}\n"
+            if note is not None:
+                hash_file += f"n:{note}\n"
+            hash_file += f"sha256:{file_sha256}\n"
+            hash_file += "HEAD:\n"
 
-        hash_file = "MFH v1\n"
-        hash_file += f"fn:{file_name}\n"
-        hash_file += f"ln:{len(file_lines)}\n"
-        hash_file += f"fs:{file_size}\n"
-        hash_file += f"sha256:{file_sha256}\n"
-        hash_file += "HEAD:\n"
-
-        for i in range(len(head_lines_data)):
-            hash_file += f"h{i+1}:{hashlib.sha256(head_lines_data[i].encode()).hexdigest()}\n"
-        hash_file += "TAIL:\n"
-        for i in range(len(tail_lines_data)):
-            hash_file += f"t{len(file_lines) - len(tail_lines_data) + i + 1}:{hashlib.sha256(tail_lines_data[i].encode()).hexdigest()}\n"
-        if return_output:
-            return hash_file
-        if output is not None:
-            with open(output, 'w') as output_file:
-                output_file.write(hash_file)
+            for i in range(len(head_lines_data)):
+                hash_file += f"h{i+1}:{hashlib.sha256(head_lines_data[i].encode()).hexdigest()}\n"
+            hash_file += "TAIL:\n"
+            for i in range(len(tail_lines_data)):
+                hash_file += f"t{len(file_lines) - len(tail_lines_data) + i + 1}:{hashlib.sha256(tail_lines_data[i].encode()).hexdigest()}\n"
+            if return_output:
+                return hash_file
+            if output is not None:
+                with open(output, 'w') as output_file:
+                    output_file.write(hash_file)
+            else:
+                print(hash_file)
         else:
-            print(hash_file)
+            json_file = {
+                "Version": "MFH v1",
+                "File Name": file_name,
+                "Line Numbers": len(file_lines),
+                "File Size": file_size,
+                "SHA256": file_sha256,
+                "Head": [],
+                "Tail": []
+            }
+            if note is not None:
+                json_file["Note"] = note
+            for i in range(len(head_lines_data)):
+                json_file["Head"].append({f"Line {i+1}": hashlib.sha256(head_lines_data[i].encode()).hexdigest()})
+            for i in range(len(tail_lines_data)):
+                json_file["Tail"].append({f"Line {len(file_lines) - len(tail_lines_data) + i + 1}": hashlib.sha256(tail_lines_data[i].encode()).hexdigest()})
+            if return_output:
+                return json_file
+            if output is not None:
+                with open(output, 'w') as output_file:
+                    json.dump(json_file, output_file, indent=4)
+            else:
+                print(json.dumps(json_file, indent=4))
+
+def parse_hash_file(hash_file):
+    hash_file = {
+        "Version": "",
+        "File Name": "",
+        "Line Numbers": 0,
+        "File Size": 0,
+        "Note": "",
+        "SHA256": "",
+        "Head": [],
+        "Tail": []
+    }
+
+    try:
+        hash_file = json.loads(hash_file)
+        return hash_file
+    except:
+        pass
+    for line in hash_file.split('\n'):
+        if line.startswith("fn:"):
+            hash_file["File Name"] = line[3:]
+        elif line.startswith("ln:"):
+            hash_file["Line Numbers"] = int(line[3:])
+        elif line.startswith("fs:"):
+            hash_file["File Size"] = int(line[3:])
+        elif line.startswith("sha256:"):
+            hash_file["SHA256"] = line[7:]
+        elif line.startswith("h"):
+            hash_file["Head"].append(line)
+        elif line.startswith("t"):
+            hash_file["Tail"].append(line)
+    return hash_file
 
 def compare(hash_a, hash_b):
-    hash_a_fn = ""
-    hash_a_ln = 0
-    hash_a_fs = 0
-    hash_a_sha256 = ""
-    hash_a_head = []
-    hash_a_tail = []
-    hash_b_fn = ""
-    hash_b_ln = 0
-    hash_b_fs = 0
-    hash_b_sha256 = ""
-    hash_b_head = []
-    hash_b_tail = []
-    for line in hash_a.split('\n'):
-        if line.startswith("fn:"):
-            hash_a_fn = line[3:]
-        elif line.startswith("ln:"):
-            hash_a_ln = int(line[3:])
-        elif line.startswith("fs:"):
-            hash_a_fs = int(line[3:])
-        elif line.startswith("sha256:"):
-            hash_a_sha256 = line[7:]
-        elif line.startswith("h"):
-            hash_a_head.append(line)
-        elif line.startswith("t"):
-            hash_a_tail.append(line)
-    for line in hash_b.split('\n'):
-        if line.startswith("fn:"):
-            hash_b_fn = line[3:]
-        elif line.startswith("ln:"):
-            hash_b_ln = int(line[3:])
-        elif line.startswith("fs:"):
-            hash_b_fs = int(line[3:])
-        elif line.startswith("sha256:"):
-            hash_b_sha256 = line[7:]
-        elif line.startswith("h"):
-            hash_b_head.append(line)
-        elif line.startswith("t"):
-            hash_b_tail.append(line)
-    if hash_a_fn != hash_b_fn:
-        print_error(f"File names do not match: {hash_a_fn} != {hash_b_fn}")
+    hash_a = parse_hash_file(hash_a)
+    hash_b = parse_hash_file(hash_b)
+
+    if hash_a["File Name"] != hash_b["File Name"]:
+        print_error(f"File names do not match: {hash_a['File Name']} != {hash_b['File Name']}")
     else:
-        print_success(f"File names match: {hash_a_fn}")
-    if hash_a_ln != hash_b_ln:
-        print_error(f"Line numbers do not match: {hash_a_ln} != {hash_b_ln}")
+        print_success(f"File names match: {hash_a['File Name']}")
+    if hash_a["Line Numbers"] != hash_b["Line Numbers"]:
+        print_error(f"Line numbers do not match: {hash_a['Line Numbers']} != {hash_b['Line Numbers']}")
     else:
-        print_success(f"Line numbers match: {hash_a_ln}")
-    if hash_a_fs != hash_b_fs:
-        print_error(f"File sizes do not match: {hash_a_fs} != {hash_b_fs}")
+        print_success(f"Line numbers match: {hash_a['Line Numbers']}")
+    if hash_a["File Size"] != hash_b["File Size"]:
+        print_error(f"File sizes do not match: {hash_a['File Size']} != {hash_b['File Size']}")
     else:
-        print_success(f"File sizes match: {hash_a_fs}")
-    if hash_a_sha256 != hash_b_sha256:
-        print_error(f"SHA256 hashes do not match: {hash_a_sha256} != {hash_b_sha256}")
+        print_success(f"File sizes match: {hash_a['File Size']}")
+    if hash_a["SHA256"] != hash_b["SHA256"]:
+        print_error(f"SHA256 hashes do not match: {hash_a['SHA256']} != {hash_b['SHA256']}")
     else:
-        print_success(f"SHA256 hashes match: {hash_a_sha256}")
-    for i in range(len(hash_a_head)):
-        if hash_a_head[i] != hash_b_head[i]:
-            print_error(f"Head hash {i+1} does not match:\t{hash_a_head[i]} != {hash_b_head[i]}")
+        print_success(f"SHA256 hashes match: {hash_a['SHA256']}")
+    for i in range(len(hash_a["Head"])):
+        if hash_a["Head"][i] != hash_b["Head"][i]:
+            print_error(f"Head hash {i+1} does not match:\t{hash_a['Head'][i]} != {hash_b['Head'][i]}")
         else:
-            print_success(f"Head hash {i+1} matches:\t{hash_a_head[i]}")
-    for i in range(len(hash_a_tail)):
-        if hash_a_tail[i] != hash_b_tail[i]:
-            print_error(f"Tail hash {i+1} does not match:\t{hash_a_tail[i]} != {hash_b_tail[i]}")
+            print_success(f"Head hash {i+1} matches:\t{hash_a['Head'][i]}")
+    for i in range(len(hash_a["Tail"])):
+        if hash_a["Tail"][i] != hash_b["Tail"][i]:
+            print_error(f"Tail hash {i+1} does not match:\t{hash_a['Tail'][i]} != {hash_b['Tail'][i]}")
         else:
-            print_success(f"Tail hash {i+1} matches:\t{hash_a_tail[i]}")
+            print_success(f"Tail hash {i+1} matches:\t{hash_a['Tail'][i]}")
 
 
 def compare_hash_file(file_path, hash_file_path):
@@ -134,6 +157,8 @@ def main():
     parser.add_argument('-o', type=str, default=None, help='Output file')
     parser.add_argument('-cf', type=str, default=None, help='Compare with other file (non hash file)')
     parser.add_argument('-ch', type=str, default=None, help='Compare with other hash file')
+    parser.add_argument('-n', type=str, default=None, help='Add note to hash file')
+    parser.add_argument('-j', type=bool, default=False, help='Output in JSON format')
     args = parser.parse_args()
 
     if args.cf is not None:
@@ -141,7 +166,7 @@ def main():
     elif args.ch is not None:
         compare_hash_file(args.file, args.ch)
     else:
-        generate_hash_file(args.file, args.hl, args.tl, args.o)
+        generate_hash_file(args.file, args.hl, args.tl, args.o, args.n)
 
 if __name__ == "__main__":
     main()
---
+++ setup.py
@@ -0,0 +1,16 @@
+# Multi file hash
+# Multi File Hash creates several hashes of the head and tail of a file.
+# GitHub: https://www.github.com/0x4248/mfh
+# Licence: GNU General Public License v3.0
+# By: 0x4248
+
+from setuptools import setup
+
+setup(
+    name="MFH",
+    version="0.1",
+    packages=["MFH"],
+    install_requires=[
+        "colorama",
+    ],
+)
